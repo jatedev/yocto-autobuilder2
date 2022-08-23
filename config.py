91b05475ceb55e0e19be1030a63abdf52e92681d
Ross Burton <ross.burton@arm.com>
2022-08-23 17:47:57 +0100

    config.py: add ubuntu2204-arm-1 (the Ampere Altra) to workers_arm
    Signed-off-by: Ross Burton <ross.burton@arm.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- config.py
+++ config.py
@@ -144,7 +144,7 @@ workers_bringup = []
 # workers with wine on them for meta-mingw
 workers_wine = ["ubuntu1804-ty-3"]
 workers_buildperf = ["perf-ubuntu1604", "perf-centos7", "perf-debian11"]
-workers_arm = ["ubuntu1804-arm-1", "ubuntu2004-arm-1"]
+workers_arm = ["ubuntu1804-arm-1", "ubuntu2004-arm-1", "ubuntu2204-arm-1"]
 # workers which don't need buildtools for AUH and are able to send email to mailing lists
 workers_auh = ["alma8-ty-1", "alma8-ty-2"]
 
